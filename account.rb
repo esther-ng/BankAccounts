beba393ed5dfad03310165081e1195a4e4860b44
Esther Ng <esther.hc.ng@gmail.com>
2016-08-24 16:44:36 -0700

    Wave 2 with functional code, need to clean up comments

--- account.rb
+++ account.rb
@@ -9,28 +9,32 @@ module Bank
   class Account
     attr_accessor :id, :balance, :owner
 
-    @@accounts = []
+    # @@accounts = []
 
-    def initialize(id, balance, opendate)
+    def initialize(id, balance, opendate, owner = nil)
       unless balance >= 0
         raise ArgumentError.new("Cannot open a new account with a negative balance.")
       end
       @owner = owner
-      @balance = Money.new(balance * 100, "USD")
+      @balance = Money.new(balance, "USD")
       @id = id
       @opendate = opendate
     end
 
+    # def self.all
+    #   return @@accounts
+    # end import_accounts(filename)
+# this both uploads the accounts from the csv file and returns it
     def self.all
-      return @@accounts
-    end
-
-    def self.import_accounts(filename)
-      @@accounts = []
-      CSV.read(filename).each do |line|
+      accounts = []
+      CSV.read('support/accounts.csv').each do |line|
         balance = Money.new(line[1], "USD")
-        @@accounts << self.new(line[0], balance, line[2])
+        accounts << self.new(line[0], balance, line[2])
       end
+      # CSV.read('support/account_owners.csv').each do |line|
+      #   (self.find(line[0])).owner = Owner.find(line[1])
+      # end
+      accounts
     end
 
     def self.find(id)
@@ -38,14 +42,26 @@ module Bank
       accounts.each do |account|
         if account.id == id
           return account
-        else
-          return "That account does not exist."
         end
       end
     end
 
+# add owner from csv to instance of account THIS WORKS!
+    def self.all_with_owners
+      self.all
+      accounts_with_names = []
+      CSV.read('support/account_owners.csv').each do |line|
+        account = self.find(line[0])
+        owner = Owner.find(line[1])
+        account.owner = owner
+        accounts_with_names << account
+      end
+      accounts_with_names
+    end
+
+
     def withdraw(amount)
-      amount = Money.new( amount * 100, "USD")
+      amount = Money.new( amount , "USD")
       unless (@balance - amount) >= 0
         raise ArgumentError.new("Cannot make a withdrawal that will result in a negative balance.")
       end
@@ -54,7 +70,7 @@ module Bank
     end
 
     def deposit(amount)
-      amount = Money.new( amount * 100, "USD")
+      amount = Money.new( amount , "USD")
       @balance += amount
       return @balance.format
     end
@@ -68,3 +84,11 @@ module Bank
     # end
   end
 end
+
+#
+#
+# def add_owner
+#   CSV.read('support/account_owners.csv').each do |line|
+#     (Bank::Account.find(line[0])).owner = accounts.find(line[1])
+#   end
+# end
--- owner.rb
+++ owner.rb
@@ -1,10 +1,33 @@
+require 'csv'
+
 module Bank
   class Owner
-    attr_accessor :name, :address
-    
-    def initialize(name, address)
-      @name = name
+    attr_accessor :id
+
+    def initialize(id, last_name, first_name, address, city, state)
+      @id = id
+      @last_name = last_name
+      @first_name = first_name
       @address = address
+      @city = city
+      @state = state
+    end
+
+    def self.all
+      owners = []
+      CSV.read('support/owners.csv').each do |line|
+        owners << self.new(line[0], line[1], line[2], line[3], line[4], line[5])
+      end
+      owners
+    end
+
+    def self.find(id)
+      owners = self.all
+      owners.each do |owner|
+        if owner.id == id
+          return owner
+        end
+      end
     end
   end
 end
