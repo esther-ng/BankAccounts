cb68dad98ceb848e7c02ec9d8c8ab92047135005
Esther Ng <esther.hc.ng@gmail.com>
2016-08-24 12:31:51 -0700

    WIP, need to match IDs in find method

--- account.rb
+++ account.rb
@@ -1,21 +1,47 @@
 require_relative 'owner'
 require 'money'
+require 'csv'
 # the find method should use the all method
 # make sure to reference the csv file in the support folder
 I18n.enforce_available_locales = false
 
 module Bank
   class Account
-    attr_accessor :balance, :owner
+    attr_accessor :id, :balance, :owner
 
-    def initialize(owner, balance)
+    @@accounts = []
+
+    def initialize(id, balance, opendate)
       unless balance >= 0
         raise ArgumentError.new("Cannot open a new account with a negative balance.")
       end
       @owner = owner
       @balance = Money.new(balance * 100, "USD")
-      id = Random.new
-      @id = id.rand(100000...999999)
+      @id = id
+      @opendate = opendate
+    end
+
+    def self.all
+      return @@accounts
+    end
+
+    def self.import_accounts(filename)
+      @@accounts = []
+      CSV.read(filename).each do |line|
+        balance = Money.new(line[1], "USD")
+        @@accounts << self.new(line[0], balance, line[2])
+      end
+    end
+
+    def self.find(id)
+      accounts = self.all
+      accounts.each do |account|
+        if account.id == id
+          return account
+        else
+          return "That account does not exist."
+        end
+      end
     end
 
     def withdraw(amount)
